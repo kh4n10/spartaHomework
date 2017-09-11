ede1df324a92b29bcb70b80d5a7d03b8d3d8a165
Abass Khan <abassk2011@hotmail.com>
2017-09-11 22:06:18 +0100

    refined switch to what I feel right, using while loops; not finishing

--- w02d01/app.js
+++ w02d01/app.js
@@ -1,8 +1,8 @@
-// The player move is gathered and stored in a variable
-    // Prompt
-// var compWin = 0;
-
+var compWin = 0;
+var playerWin = 0;
 // for (var playerWin = 0; playerWin <=10; playerWin ++) {
+
+while (compWin <= 10 || playerWin <= 10) {
 
 var playerChoice = prompt("Make a choice (r)ock, (p)apers or (s)cissors");
 
@@ -20,28 +20,69 @@ var computerChoice = Math.floor(Math.random()*3);
  // Tell the user the outcome of the game
   // Alert
 
-
-switch (computerChoice) {
-	case if(playerChoice === "r" && computerChoice > 0):
-		alert("its a draw; computer chose rock");
-		alert("you lose; computer chose paper");
-		alert("you win; computer chose scissors");
-		break;
-
-	case 1:
-		if(playerChoice === "p")
-		alert("its a draw; computer chose paper");
-		alert("you lose; computer chose scissors");
-		alert("you win; computer chose rock");
-		break;
-
-	case 2:
-		if(playerChoice === "s")
-		alert("its a draw; computer chose scissors");
-		alert("you lose; computer chose rock");
-		alert("you win; computer chose paper");
-		break;
+if(playerChoice === "r") {
+	switch (computerChoice) {
+		case 0:
+			alert("its a draw; computer chose rock");
+			break;
+	
+		case 1:
+			alert("you lose; computer chose paper");
+			compWin = compWin + 1;
+			alert("computer:"+compWin+ "player:"+playerWin);			
+			break;
+	
+		case 2:
+			alert("you win; computer chose scissors");
+			playerWin = playerWin + 1;
+			alert("computer:"+compWin+ "player:"+playerWin);
+			break;
+	}
 }
+
+
+if(playerChoice === "s") {
+	switch (computerChoice) {
+		case 0:
+			alert("you lose; computer chose rock");
+			compWin = compWin + 1;
+			alert("computer:"+compWin+ "player:"+playerWin);
+			break;
+	
+		case 1:
+			alert("you win; computer chose paper");
+			playerWin = playerWin + 1;
+			alert("computer:"+compWin+ "player:"+playerWin);
+			break;
+	
+		case 2:
+			alert("its a draw; computer chose scissors");
+			break;
+	}
+}
+
+
+if(playerChoice === "p") {
+	switch (computerChoice) {
+		case 0:
+			alert("you win; computer chose rock");
+			playerWin = playerWin + 1;
+			alert("computer:"+compWin+ "player:"+playerWin);
+			break;
+	
+		case 1:
+			alert("its a draw; computer chose paper");
+			break;
+	
+		case 2:
+			alert("you lose; computer chose scissors");
+			compWin = compWin + 1;
+			alert("computer:"+compWin+ "player:"+playerWin);
+			break;
+	}
+}
+}
+
 
 // if(playerChoice ==="r" && computerChoice ===2) {
 // 	alert("player won");
