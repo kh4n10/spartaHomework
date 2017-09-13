2771f1e14c6659a5b4a2014cec91435d18bc188e
Abass Khan <abassk2011@hotmail.com>
2017-09-13 10:38:22 +0100

    presented previous version to class, this version altered significantly; use of arrays & renaming variables

--- w02d02/js-dom-checkers/app.js
+++ w02d02/js-dom-checkers/app.js
@@ -1,4 +1,4 @@
-
+var whiteBoxes = [0, 2, 4, 6, 9, 11, 13, 15, 16, 18, 20, 22, 25, 27, 29, 31, 32, 34, 36, 38, 41, 43, 45, 47, 48, 50, 52, 54, 57, 59, 61, 63];
 
 
 
@@ -11,15 +11,15 @@ function addCheckersBox () {
 
 
 for (var i = 0; i < 64; i++) {
-	addCheckersBox("");
+	addCheckersBox();
 }
 
-var whiteBox = document.getElementsByTagName("li")
-for (var i = 0; i < 64; i++) {
-	whiteBox[i].className = "white"
-}if (i )
-
-var blackBox = document.getElementsByTagName("li")
-for (var i = 0; i < 64; i+=2) {
-	blackBox[i].className = "black"
+var boxes = document.getElementsByTagName("li")
+for (var i = 0; i < 64 ; i++) {
+	boxes[i].className = "black"
+	if (whiteBoxes.indexOf(i) !== -1) {
+		boxes[i].className = "white"
+	}
 }
+
+
