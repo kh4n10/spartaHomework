69fffa67ca88e10d7ec44ebeecb87032b77b304a
Abass Khan <abassk2011@hotmail.com>
2017-09-25 21:25:22 +0100

    use of while loops to do the same thing

--- w04d01/calculator.rb
+++ w04d01/calculator.rb
@@ -28,44 +28,44 @@ end
 puts "(b)asic, (ad)vanced"
 choice = gets.chomp
 
-# choice of operation in basic
-if choice === "b" then puts "choose if you want to (a)dd, (s)ubtract, (d)ivide or (m)ultiply"
-	choice = gets.chomp
-	num1 = gets.chomp.to_f
-	num2 = gets.chomp.to_f
-	case choice
-	when "a"
-		add(num1, num2)
-	when "s"
-		subtract(num1, num2)
-	when "m"
-		multiply(num1, num2)
-	when "d"
-		divide(num1, num2)
-	end
-else puts "Error, select (b)asic or (ad)vance"
-end
+# choices
 
-# choice of operation in advanced
-if choice === "ad" then puts "choose if you want to (a)dd, (s)ubtract, (d)ivide, (m)ultiply, (p)ower or (sq)uare root"
-	choice = gets.chomp
-	num1 = gets.chomp.to_f
-	num2 = gets.chomp.to_f
-	case choice
-	when "a"
-		add(num1, num2)
-	when "s"
-		subtract(num1, num2)
-	when "m"
-		multiply(num1, num2)
-	when "d"
-		divide(num1, num2)
-	when "p"
-		powers(num1, num2)
-	when "sq"
-		square_root(num1, num2)
-	end
-end
+# if choice === "b" then puts "choose if you want to (a)dd, (s)ubtract, (d)ivide or (m)ultiply"
+# 	choice = gets.chomp
+# 	num1 = gets.chomp.to_f
+# 	num2 = gets.chomp.to_f
+# 	case choice
+# 	when "a"
+# 		add(num1, num2)
+# 	when "s"
+# 		subtract(num1, num2)
+# 	when "m"
+# 		multiply(num1, num2)
+# 	when "d"
+# 		divide(num1, num2)
+# 	end
+# elsif choice === "ad" then puts "choose if you want to (a)dd, (s)ubtract, (d)ivide, (m)ultiply, (p)ower or (sq)uare root"
+# 	choice = gets.chomp
+# 	num1 = gets.chomp.to_f
+# 	num2 = gets.chomp.to_f
+# 	case choice
+# 	when "a"
+# 		add(num1, num2)
+# 	when "s"
+# 		subtract(num1, num2)
+# 	when "m"
+# 		multiply(num1, num2)
+# 	when "d"
+# 		divide(num1, num2)
+# 	when "p"
+# 		powers(num1, num2)
+# 	when "sq"
+# 		square_root(num1, num2)
+# 	end
+# elsif choice != "b" || choice != "ad"
+# 	puts "Error"
+# end
+
 
 	# choice = gets.chomp
 	# num1 = gets.chomp.to_f
@@ -91,5 +91,43 @@ end
 # 	powers(num1, num2)
 # elsif choice === "sq"
 # 	square_root(num1)
+# end
+
+# while choice === "b"
+# 	puts "choose if you want to (a)dd, (s)ubtract, (d)ivide or (m)ultiply"
+# 	choice = gets.chomp
+# 	num1 = gets.chomp.to_f
+# 	num2 = gets.chomp.to_f
+# 	case choice
+# 	when "a"
+# 		add(num1, num2)
+# 	when "s"
+# 		subtract(num1, num2)
+# 	when "m"
+# 		multiply(num1, num2)
+# 	when "d"
+# 		divide(num1, num2)
+# 	end
+# end
+
+# while choice === "ad"
+# 	puts "choose if you want to (a)dd, (s)ubtract, (d)ivide, (m)ultiply, (p)ower or (sq)uare root"
+# 	choice = gets.chomp
+# 	num1 = gets.chomp.to_f
+# 	num2 = gets.chomp.to_f
+# 	case choice
+# 	when "a"
+# 		add(num1, num2)
+# 	when "s"
+# 		subtract(num1, num2)
+# 	when "m"
+# 		multiply(num1, num2)
+# 	when "d"
+# 		divide(num1, num2)
+# 	when "p"
+# 		powers(num1, num2)
+# 	when "sq"
+# 		square_root(num1)
+# 	end
 # end
 
