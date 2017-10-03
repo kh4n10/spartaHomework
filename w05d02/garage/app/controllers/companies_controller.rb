aeac5bcbf8e7c8e655dd72b7ab2ac052f1c48ba1
Abass Khan <abassk2011@hotmail.com>
2017-10-03 16:42:41 +0100

    defining the restful routes in the company controller and adding to the views

--- w05d02/garage/app/controllers/companies_controller.rb
+++ w05d02/garage/app/controllers/companies_controller.rb
@@ -1,5 +1,47 @@
 class CompaniesController < ApplicationController
+# below links to the private method and allows the show, edit to be empty
+before_action :set_company, only: [:show, :edit, :update, :destroy]
 
+
+	def index
+  	@companies = Company.all
+  end
 	
-	
+	def show
+	end
+
+	def new
+		@company = Company.new
+	end
+
+	def edit
+	end
+
+	def create
+  	@company = Company.new(params[:name], params[:established], params[:description])
+
+		@company.save
+    redirect_to companies_url, notice: 'Company was successfully created.'
+  end
+
+
+  def update
+  end
+
+
+  def destroy
+  	@company.destroy
+  	redirect_to companies_url
+  end
+
+
+
+
+  private
+    
+    def set_company
+      @company = Company.find(params[:id])
+    end
+
+
 end
